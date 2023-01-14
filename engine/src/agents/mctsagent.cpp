8c890968ea38957e1bead345eee4bdaa591b9b16
KingEsel1 <reutermarkus1@gmail.com>
2023-01-14 14:33:42 +0100

    calculation of novelty score v3

--- engine/src/agents/mctsagent.cpp
+++ engine/src/agents/mctsagent.cpp
@@ -180,8 +180,9 @@ void MCTSAgent::create_new_root_node(StateObj* state)
     state->get_state_planes(true, inputPlanes, net->get_version());
     net->predict(inputPlanes, valueOutputs, probOutputs, auxiliaryOutputs);
     size_t tbHits = 0;
+    //MR add inputPlanes to params
     fill_nn_results(0, net->is_policy_map(), valueOutputs, probOutputs, auxiliaryOutputs, rootNode.get(), tbHits,
-                    rootState->mirror_policy(state->side_to_move()), searchSettings, rootNode->is_tablebase());
+                    rootState->mirror_policy(state->side_to_move()), searchSettings, rootNode->is_tablebase(), inputPlanes);
 #endif
     rootNode->prepare_node_for_visits();
 }
