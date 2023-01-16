c7a0fbd7e2ed13952cdb0907fd33db7fdbc2f4d1
KingEsel1 <reutermarkus1@gmail.com>
2023-01-16 12:10:39 +0100

    correct backprop v2

--- engine/src/node.h
+++ engine/src/node.h
@@ -213,6 +213,9 @@ public:
             assert(!isnan(d->qValues[childIdx]));
 
             //MR noveltyScore bekommt kein virtualLoss... Mittelwert
+            if (noveltyScore > 1.0f || noveltyScore < -1.0f) {
+                noveltyScore = 0.0f;
+            }
             d->noveltyScores[childIdx] = (double(d->noveltyScores[childIdx]) * d->childNumberVisits[childIdx] + noveltyScore) / d->childNumberVisits[childIdx];
             info_string("//MR                                                                  noveltyScore nach Backprop: " + to_string(d->noveltyScores[childIdx]));
             assert(!isnan(d->noveltyScores[childIdx]));            
