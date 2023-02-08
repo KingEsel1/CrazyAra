612c9dcf05f1392da932ee5917d9497341713af5
KingEsel1 <reutermarkus1@gmail.com>
2023-02-08 11:27:13 +0100

    debug with martin v1

--- engine/src/uci/crazyara.cpp
+++ engine/src/uci/crazyara.cpp
@@ -235,11 +235,11 @@ void CrazyAra::wait_to_finish_last_search()
 void CrazyAra::stop_search()
 {
     if (mctsAgent != nullptr) {
-        //info_string("//MR: in stop_search() and mctsAgent not nullptr. Before mctsAgent->stop()");
+        info_string("//MR: in stop_search() and mctsAgent not nullptr. Before mctsAgent->stop()");
         mctsAgent->stop();
-        //info_string("//MR: After mctsAgent->stop() and before wait_to_finish_last_search()");
+        info_string("//MR: After mctsAgent->stop() and before wait_to_finish_last_search()");
         wait_to_finish_last_search();
-        //info_string("//MR: After  wait_to_finish_last_search()");
+        info_string("//MR: After  wait_to_finish_last_search()");
     }
 }
 
