6e91ae65e262f403a9eb0236284a5bec82eb9f45
KingEsel1 <reutermarkus1@gmail.com>
2023-03-07 01:56:59 +0100

    activated pseudocount novelty v2

--- engine/src/searchthread.h
+++ engine/src/searchthread.h
@@ -198,12 +198,12 @@ private:
 };
 
 void run_search_thread(SearchThread *t);
-//MR add inputPlanes to params
+//MR
 void fill_nn_results(size_t batchIdx, bool isPolicyMap, const float* valueOutputs, const float* probOutputs, const float* auxiliaryOutputs, Node *node, size_t& tbHits, bool mirrorPolicy, const SearchSettings* searchSettings, bool isRootNodeTB, const float* inputPlanes, float* factPlanes, int numberInputTotal, float timeStep, float featureProbabilities); //MR-pseudo 
 void node_post_process_policy(Node *node, float temperature, const SearchSettings* searchSettings);
 void node_assign_value(Node *node, const float* valueOutputs, size_t& tbHits, size_t batchIdx, bool isRootNodeTB);
 //MR
-void node_assign_novelty_score(Node *node, const float* valueOutputs, size_t batchIdx, const SearchSettings* searchSettings, const float* inputPlanes, float* factPlanes, int numberInputTotal, timeStep, featureProbabilities); //MR-pseudo
+void node_assign_novelty_score(Node *node, const float* valueOutputs, size_t batchIdx, const SearchSettings* searchSettings, const float* inputPlanes, float* factPlanes, int numberInputTotal, float timeStep, float featureProbabilities); //MR-pseudo
 
 /**
  * @brief random_root_playout Uses random move exploration (epsilon greedy) from the given position. The probability for doing a random move decays by depth.
